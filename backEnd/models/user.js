048015e5e881854caa1124f8e3399beb97b863c9
siwarrihane <siwarrihane03@gmail.com>
2023-11-24 19:47:08 +0100

    test

--- backEnd/models/user.js
+++ backEnd/models/user.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         username: DataTypes.STRING,
         userLastname:DataTypes.STRING,
-        age: DataTypes.STRING,
+       
         password:DataTypes.STRING,
         email: DataTypes.STRING,
         role: {
